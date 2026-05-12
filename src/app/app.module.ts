1d814fead07df40ac1b6e1caff4c0eff31298cac
juanmorenaso15 <juanjosemorenobenavides207@gmail.com>
2026-05-12 07:06:11 -0500

    fead: se añado metodos para el componente boton-numero, y se añade el compornenete boton-operacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { PantallaComponent } from './components/pantalla/pantalla.component';
 import { BotonComponent } from './components/boton/boton.component';
+import { BotonNumeroComponent } from './components/boton-numero/boton-numero.component';
+import { BotonOperacionComponent } from './components/boton-operacion/boton-operacion.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PantallaComponent,
-    BotonComponent
+    BotonComponent,
+    BotonNumeroComponent,
+    BotonOperacionComponent
   ],
   imports: [
     BrowserModule, FormsModule
---
+++ src/app/components/boton-operacion/boton-operacion.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-boton-operacion',
+  templateUrl: './boton-operacion.component.html',
+  styleUrl: './boton-operacion.component.scss'
+})
+export class BotonOperacionComponent {
+
+}
